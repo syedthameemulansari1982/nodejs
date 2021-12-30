a6cb2703c08e3712aae2360b210b6db98fa390c3
syedthameemulansari1982 <96604581+syedthameemulansari1982@users.noreply.github.com>
2021-12-30 23:02:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("IT WORKED");
+  res.send("IT STARTED WORKING WITH SAMPLE OUTPUT");
 });
 app.get("/new", function (req, res) {
   res.send("New also worked");
