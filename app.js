7c0abdd83139205129a16ab6709778ff3ad406e1
syedthameemulansari1982 <96604581+syedthameemulansari1982@users.noreply.github.com>
2021-12-30 22:27:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.get("/", function (req, res) {
 app.get("/new", function (req, res) {
   res.send("New also worked");
 });
-
+app.get("/new/sampleapp", function (req, res) {
+  res.send("New sample application also worked");
+});
 app.listen(process.env.PORT || 5000);
 module.exports = app;
